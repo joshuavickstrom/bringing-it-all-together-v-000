0d3afd8871e6773c5d9b18cf1b2b27ec179712f0
Joshua Vickstrom <joshuavickstrom@outlook.com>
2018-02-27 17:39:51 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,12 @@
 require 'pry'
 
 class Dog
-
-attr_accessor :name, :breed
-
-def initialize(id: id, name: name, breed: breed)
-  @id = id
-  @name = name
-  @breed = breed
-end
+  
+  attr_accessor :name, :breed
+  
+  def initialize(id: id, name: name, breed: breed)
+    @id = id
+    @name = name
+    @breed = breed
+  end
 end
