5846ee71986677e91265dcd9db0c1b4e8b5f135d
Joshua Vickstrom <joshuavickstrom@outlook.com>
2018-02-27 19:57:52 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -69,13 +69,15 @@ class Dog
     DB[:conn].execute(sql, id).map { |array| self.new_from_db(array) }.first
   end
 
-  def self.find_or_create_by(??)
-    sql = <<-SQL 
-      
-      
-    SQL
-    
-    DB[:conn].execute(sql)
+  def self.find_or_create_by(name:, breed:)
+    if DB[:conn].
+      self.create
+        sql = <<-SQL
+          
+
+        SQL
+
+        DB[:conn].execute(sql)
   end
 
 end
