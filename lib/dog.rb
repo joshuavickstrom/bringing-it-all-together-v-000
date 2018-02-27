5dffbef2f38e8a89d0e2324967372e19595ecb09
Joshua Vickstrom <joshuavickstrom@outlook.com>
2018-02-27 18:05:38 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -53,8 +53,8 @@ class Dog
     new_dog = Dog.new(array[0], array[1], array[2])
   end
 
-  def self.create(name:[1], breed:[2])
-    new_dog = Dog.new(name, breed)
+  def self.create(name:name, breed:breed)
+    new_dog = Dog.new(name[1], breed[2])
     new_dog.save
     new_dog
   end
