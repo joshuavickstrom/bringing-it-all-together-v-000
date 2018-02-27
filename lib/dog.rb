3045a60203b5d7dd8073213822d3992a3173097a
Joshua Vickstrom <joshuavickstrom@outlook.com>
2018-02-27 18:53:35 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -59,12 +59,12 @@ class Dog
     new_dog
   end
 
-  def self.find_by_id(id)
+  def self.find_by_id(id:)
     sql = <<-SQL
       SELECT *
       FROM dogs
       WHERE id =?
     SQL
-    DB[:conn].execute(sql, self.name, self.breed, self.id)
+    DB[:conn].execute(sql, self.id)
   end
 end
