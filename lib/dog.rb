60a53cecdbb95225a332764cd32de575e2e01595
Joshua Vickstrom <joshuavickstrom@outlook.com>
2018-02-27 20:20:45 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -84,6 +84,12 @@ class Dog
 
   def self.find_by_name(name)
     sql = <<-SQL
+    SELECT *
+    FROM dogs
+    WHERE name =?
+    LIMIT 1
     SQL
+
+    DB[:conn].execute(sql, name).map { |array| self.new_from_db(array)  }.first
   end
 end
